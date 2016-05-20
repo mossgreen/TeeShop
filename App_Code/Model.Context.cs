b1c07242a96b19aea5b7408177965f58d33e120a
Moss Green <gufeifei@outlook.com>
2016-05-20 19:46:56 +1200

    tested

--- App_Code/Model.Context.cs
+++ App_Code/Model.Context.cs
@@ -27,4 +27,5 @@ public partial class TeeShopEntities : DbContext
     public virtual DbSet<Product> Products { get; set; }
     public virtual DbSet<ProductType> ProductTypes { get; set; }
     public virtual DbSet<Client> Clients { get; set; }
+    public virtual DbSet<Order> Orders { get; set; }
 }
